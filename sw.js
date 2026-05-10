1e2605e476b5975e883997d45e655a552270dbe1
Cristiano252525 <cristianomendesdejesus25@gmail.com>
2026-05-09 22:06:09 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,8 @@
 
+self.addEventListener('install', (e) => {
+  console.log('X-APK Service Worker Instalado');
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request));
+});
